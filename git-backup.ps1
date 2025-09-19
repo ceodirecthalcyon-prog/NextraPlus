2697ccaf810fdb399c8965f19af5fb30227abaef
P.Santra <ceo.direct.halcyon@gmail.com>
2025-09-19 18:11:10 +0530

    Test backup - 2025-09-19_18-11-10

---
+++ git-backup.ps1
@@ -0,0 +1,14 @@
+﻿param([string]$msg = "Auto backup")
+
+# Go to project folder
+Set-Location "E:\NextraPlus"
+
+# Stage files
+git add .
+
+# Commit with timestamp
+$time = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+git commit -m "$msg - $time"
+
+# Push to GitHub
+git push origin main
